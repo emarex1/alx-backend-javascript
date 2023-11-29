e6f382cffd0c9bbad8ae15cf32ed6ac5ae28cedd
emarex1 <emmanuelawoh0@gmail.com>
2023-11-29 06:04:04 +0100

    return a promise

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,17 +1,4 @@
-// 0-promise.js
-
-function getResponseFromAPI() {
-    return new Promise((resolve, reject) => {
-        // Your API call or asynchronous operation goes here
-        // For example, a simple setTimeout to simulate an asynchronous operation
-        setTimeout(() => {
-            // Assuming some data is retrieved successfully
-            const data = { message: "API response data" };
-
-            // Resolve the promise with the data
-            resolve(data);
-        }, 1000); // Simulating a delay of 1000 milliseconds
-    });
+/* eslint-disable */
+export default function getResponseFromAPI() {
+    return (new Promise((resolve, reject) => { }));
 }
-
-export default getResponseFromAPI;
